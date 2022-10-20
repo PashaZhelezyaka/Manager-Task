4fdafdd88271e0cada0f63a16ea358138db91d0d
PashaZhelezyaka <pilipenkopv1991@gmail.com>
2022-10-20 17:39:38 +0300

    task list, drop task

--- src/app/components/task-list/task-list.component.ts
+++ src/app/components/task-list/task-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { TaskList } from "../../../interface/interface";
+
 
 @Component({
   selector: 'app-task-list',
@@ -10,6 +13,16 @@ export class TaskListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  tasks: TaskList[] = [
+    {id: Date.now() ,title: "task1", description: "learn ng", date:"2022-10-12"},
+    {id: Date.now() ,title: "task2", description: "learn ts", date:"2022-10-12"},
+    {id: Date.now() ,title: "task3", description: "learn ngrx", date:"2022-10-12"},
+  ];
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.tasks, event.previousIndex, event.currentIndex);
   }
 
 }
